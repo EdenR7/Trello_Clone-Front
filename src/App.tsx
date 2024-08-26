ece7b529f57df1bbc1ab8c70ff5406a49d30d678
NatiCohen10 <nati1236nati@gmail.com>
2024-08-26 21:35:12 +0300

    changed app layout, fixed problem with npm install(?)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import {
-  createBrowserRouter,
-  Navigate,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import PlatformLayout from "./layouts/platform-layout";
 import AuthLayout from "./layouts/auth-layout";
@@ -10,105 +6,80 @@ import AuthLayout from "./layouts/auth-layout";
 import LoginPage from "./pages/login-page";
 import RegisterPage from "./pages/register-page";
 import AboutPage from "./pages/about-page";
-
-import { useAuth } from "./providers/auth-provider";
 import TeamPage from "./pages/vision-page";
 import VisionPage from "./pages/team-page";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <PlatformLayout />,
-    children: [
-      {
-        index: true,
-        element: <div>Home</div>,
-      },
+import { useAuth } from "./providers/auth-provider";
+import { ReactNode } from "react";
 
-      {
-        path: "about",
-        element: <AboutPage />,
-        children: [
-          {
-            path: "team",
-            element: <TeamPage />,
-          },
-          {
-            path: "vision",
-            element: <VisionPage />,
-          },
-        ],
-      },
-      {
-        path: "contact",
-        element: <div>Contact</div>,
-      },
-      {
-        path: "services",
-        element: <div>Services</div>,
-      },
-      {
-        path: "protected",
-        element: <ProtectedRoute>Protected</ProtectedRoute>,
-      },
-    ],
-  },
-  {
-    path: "auth",
-    element: (
-      <AuthRoutes>
-        <AuthLayout />
-      </AuthRoutes>
-    ),
-    children: [
-      {
-        path: "login",
-        element: <LoginPage />,
-      },
-      {
-        path: "register",
-        element: <RegisterPage />,
-      },
-    ],
-  },
-]);
+interface customRouteProps {
+  children: ReactNode;
+}
 
-function ProtectedRoute({ children }: { children: React.ReactNode }) {
+function ProtectedRoute({ children }: customRouteProps) {
   const { loggedInUser } = useAuth();
 
-  // in case the user is still being fetched
   if (loggedInUser === undefined) {
     return null;
   }
 
-  // if the user is not logged in, redirect
   if (loggedInUser === null) {
     return <Navigate to="/auth/login" replace />;
   }
 
-  // if the user is logged in, show the protected route
   return children;
 }
 
-function AuthRoutes({ children }: { children: React.ReactNode }) {
+function AuthRoutes({ children }: customRouteProps) {
   const { loggedInUser } = useAuth();
 
-  // in case the user is still being fetched
   if (loggedInUser === undefined) {
     return null;
   }
 
-  // if the user is logged in, redirect to home
   if (loggedInUser) {
     return <Navigate to="/" replace />;
   }
 
-  // if the user is not logged in, show the auth routes
   return children;
 }
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PlatformLayout />}>
+          <Route index element={<div>Home</div>} />
+          <Route path="about" element={<AboutPage />}>
+            <Route path="team" element={<TeamPage />} />
+            <Route path="vision" element={<VisionPage />} />
+          </Route>
+          <Route path="contact" element={<div>Contact</div>} />
+          <Route path="services" element={<div>Services</div>} />
+          <Route
+            path="protected"
+            element={
+              <ProtectedRoute>
+                <div>Protected</div>
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+
+        <Route
+          path="auth"
+          element={
+            <AuthRoutes>
+              <AuthLayout />
+            </AuthRoutes>
+          }
+        >
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
