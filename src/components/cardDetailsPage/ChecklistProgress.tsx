c78b4d15994901d03f009a8e01ef6cf281b880d3
NatiCohen10 <nati1236nati@gmail.com>
2024-08-29 18:03:12 +0300

    added progress bar dynamic color change, added number of checked items, added seperate states for each checklist

--- src/components/cardDetailsPage/ChecklistProgress.tsx
+++ src/components/cardDetailsPage/ChecklistProgress.tsx
@@ -1,18 +1,25 @@
 import { Progress } from "../ui/progress";
-import { ITodo } from "@/types/checklist.types";
+import { IChecklist, ITodo } from "@/types/checklist.types";
 
 interface ChecklistProgressProps {
-  todos: ITodo[];
+  checklist: IChecklist | undefined;
 }
 
-export default function ChecklistProgress({ todos }: ChecklistProgressProps) {
+export default function ChecklistProgress({
+  checklist,
+}: ChecklistProgressProps) {
+  let todos;
+  if (checklist?.todos) {
+    todos = checklist.todos;
+  }
+
   const calculateProgress = (todos: ITodo[]) => {
     if (todos.length === 0) return 0;
     const completedTodos = todos.filter((todo) => todo.isComplete).length;
     return (completedTodos / todos.length) * 100;
   };
 
-  const progress = calculateProgress(todos);
+  const progress = calculateProgress(todos!);
 
   return (
     <div className="relative mb-[6px]">
@@ -21,7 +28,8 @@ export default function ChecklistProgress({ todos }: ChecklistProgressProps) {
       </span>
       <Progress
         value={progress}
-        className="ml-10 relative overflow-hidden h-2 rounded-md bg-btn_bg_primary"
+        indicatorColor={`${progress === 100 ? "green-700" : "primary"}`}
+        className="ml-10 relative overflow-hidden h-2 rounded-md bg-btn_bg_primary  "
       />
     </div>
   );
---
+++ src/hooks/Query hooks/Todo hooks/useToggleTodoComplete.ts
@@ -0,0 +1,63 @@
+import api from "@/lib/api";
+import { ICard } from "@/types/card.types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+interface toggleTodoParams {
+  cardId: string;
+  checklistId: string;
+  todoId: string;
+}
+
+async function toggleTodoComplete(
+  cardId: string,
+  checklistId: string,
+  todoId: string
+) {
+  const res = await api.patch(`/card/${cardId}/checklist/toggleComplete`, {
+    checklistId,
+    todoId,
+  });
+  return res.data;
+}
+
+export function useToggleTodoComplete(boardId: string) {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ cardId, checklistId, todoId }: toggleTodoParams) =>
+      toggleTodoComplete(cardId, checklistId, todoId),
+    onMutate: async ({ cardId, checklistId, todoId }) => {
+      await queryClient.cancelQueries({ queryKey: ["card", cardId] });
+
+      const previousCard = queryClient.getQueryData<ICard>(["card", cardId]);
+      if (previousCard) {
+        const updatedCard = {
+          ...previousCard,
+          checklist: previousCard.checklist.map((checklist) =>
+            checklist._id === checklistId
+              ? {
+                  ...checklist,
+                  todos: checklist.todos.map((todo) =>
+                    todo._id === todoId
+                      ? { ...todo, isComplete: !todo.isComplete }
+                      : todo
+                  ),
+                }
+              : checklist
+          ),
+        };
+
+        queryClient.setQueryData(["card", cardId], updatedCard);
+      }
+      return { previousCard };
+    },
+    onError: (_, { cardId }, context) => {
+      if (context?.previousCard) {
+        queryClient.setQueryData<ICard>(["card", cardId], context.previousCard);
+      }
+    },
+    onSettled: (_, __, { cardId }) => {
+      queryClient.invalidateQueries({ queryKey: ["card", cardId] });
+      queryClient.invalidateQueries({ queryKey: ["board", boardId] });
+    },
+  });
+}
