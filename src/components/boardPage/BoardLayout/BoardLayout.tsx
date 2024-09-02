e4542cd8198fb8e4848ddcf977802f0cdde7ae70
EdenR7 <edenrot777@gmail.com>
2024-09-02 21:55:36 +0300

    Eden

--- src/components/boardPage/BoardLayout/BoardLayout.tsx
+++ src/components/boardPage/BoardLayout/BoardLayout.tsx
@@ -1,11 +1,12 @@
 import { Outlet, useParams } from "react-router-dom";
 import { useGetBoard } from "@/hooks/Query hooks/Board hooks/useGetBoard";
 import BoardItems from "../BoardItems";
-import { Ellipsis, ListFilter } from "lucide-react";
+import { Ellipsis, ListFilter, Star } from "lucide-react";
 import BoardSideBar from "./RightSideBar/BoardSideBar";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { IBoardBackground } from "@/types/board.types";
+import { useAuth } from "@/providers/auth-provider";
 
 export type BoardStyle = {
   backgroundColor?: string;
@@ -35,13 +36,14 @@ export function getBoardBgStyle(bg: IBoardBackground) {
 }
 
 function BoardLayout() {
+  const { loggedInUser } = useAuth();
   const { boardId } = useParams();
   const { data: board, isPending, isError, error } = useGetBoard(boardId!);
   const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  const isStarred = loggedInUser?.sttaredBoards.find((b) => b._id === boardId);
 
   if (!board) return null;
   const boardStyle = getBoardBgStyle(board.bg);
-  // console.log("hi");
 
   if (isPending) return <div>Loadinggg....</div>;
   if (isError) return <div>Error: {error.message}</div>;
@@ -51,21 +53,51 @@ function BoardLayout() {
         style={boardStyle!}
         className="h-[calc(100vh-48px)] w-full relative overflow-y-hidden"
       >
-        <div className=" backdrop-blur-[4px] supports-[backdrop-filter]:bg-background/80">
-          <div className=" py-3 pr-[10px] pl-4 flex justify-between">
-            <div className=" flex items-center">a</div>
+        <div
+          className={`text-white backdrop-blur-[4px] supports-[backdrop-filter]:bg-black/[.10] ${
+            isSideBarOpen && "mr-[300px] md:mr-[340px]"
+          }`}
+        >
+          <div className=" py-3 pr-[10px] pl-4 flex justify-between flex-wrap">
+            <div className=" flex-1 min-w-12 mr-1 flex items-center flex-nowrap">
+              <h1 className=" truncate px-[10px] font-bold text-lg">
+                {board.name}
+              </h1>
+              <Button
+                variant={"naked"}
+                className=" bg-inherit p-0 m-0 hover:bg-inherit group"
+                size={"sm"}
+              >
+                {isStarred ? (
+                  <Star
+                    fill="currentColor"
+                    size={16}
+                    className=" transition-transform duration-200 ease-in-out group-hover:scale-125"
+                  />
+                ) : (
+                  <Star
+                    size={16}
+                    className=" transition-transform duration-200 ease-in-out group-hover:scale-125"
+                  />
+                )}
+              </Button>
+            </div>
             <div className=" flex items-center gap-2">
-              <span>
-                <ListFilter size={16} color="black" />
-              </span>
-              <span className=" h-5 w-[1px] bg-gray-500 rounded-xl opacity-35"></span>
+              <Button
+                className=" py-[6px] px-2 hover:bg-white hover:bg-opacity-10"
+                variant="naked"
+                // onClick={() => setIsSideBarOpen(true)}
+              >
+                <ListFilter size={18} color="white" />
+              </Button>
+              <span className=" h-5 w-[1px] bg-gray-400 rounded-xl opacity-35"></span>
               <div>
                 <Button
-                  className=" py-[6px] px-2"
+                  className=" py-[6px] px-2 hover:bg-white hover:bg-opacity-10"
                   variant="naked"
                   onClick={() => setIsSideBarOpen(true)}
                 >
-                  <Ellipsis size={18} />
+                  <Ellipsis size={18} color="white" />
                 </Button>
               </div>
             </div>
