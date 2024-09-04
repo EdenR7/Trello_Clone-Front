fde9d2df53c4673393f47cb9c97a5d44aad29a61
NatiCohen10 <nati1236nati@gmail.com>
2024-09-04 12:26:31 +0300

    changed gradients to be to bottom right

--- src/constants/gradient.constant.ts
+++ src/constants/gradient.constant.ts
@@ -12,39 +12,39 @@
 
 export const gradientStrings = [
   {
-    linear: "linear-gradient(to bottom, #0C66E4 2%, #37B4C3 100%)",
+    linear: "linear-gradient(to bottom right, #0C66E4 2%, #37B4C3 100%)",
     emoji: "❄️",
   }, // ocean
   {
-    linear: "linear-gradient(to bottom, #0C66E4 2%, #09326C 100%)",
+    linear: "linear-gradient(to bottom right, #0C66E4 2%, #09326C 100%)",
     emoji: "🌊",
   }, // space
   {
-    linear: "linear-gradient(to bottom, #09326C 2%, #CD519D 100%)",
+    linear: "linear-gradient(to bottom right, #09326C 2%, #CD519D 100%)",
     emoji: "🔮",
   }, // sunset
   {
-    linear: "linear-gradient(to bottom, #6E5DC6 2%, #E774BB 100%)",
+    linear: "linear-gradient(to bottom right, #6E5DC6 2%, #E774BB 100%)",
     emoji: "🌈",
   }, // galaxy
   {
-    linear: "linear-gradient(to bottom, #E34935 2%, #FAA53D 100%)",
+    linear: "linear-gradient(to bottom right, #E34935 2%, #FAA53D 100%)",
     emoji: "🍊",
   }, // lava
   {
-    linear: "linear-gradient(to bottom, #E774BB 2%, #F87462 100%)",
+    linear: "linear-gradient(to bottom right, #E774BB 2%, #F87462 100%)",
     emoji: "🌸",
   }, // rose
   {
-    linear: "linear-gradient(to bottom, #1F845A 2%, #60C6D2 100%)",
+    linear: "linear-gradient(to bottom right, #1F845A 2%, #60C6D2 100%)",
     emoji: "🌎",
   }, // tropical
   {
-    linear: "linear-gradient(to bottom, #505F79 2%, #172B4D 100%)",
+    linear: "linear-gradient(to bottom right, #505F79 2%, #172B4D 100%)",
     emoji: "👽",
   }, // twilight
   {
-    linear: "linear-gradient(to bottom, #43290F 2%, #AE2A19 100%)",
+    linear: "linear-gradient(to bottom right, #43290F 2%, #AE2A19 100%)",
     emoji: "🌋",
   }, // desert
 ];
